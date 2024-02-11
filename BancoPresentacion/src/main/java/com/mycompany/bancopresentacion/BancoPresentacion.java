b4b62a8c6996f6212dcbc111f752486f7ed901fc
Carlos <carloshiramsanchez@gmail.com>
2024-02-11 16:49:02 -0700

    Created BancoPresentacion

---
+++ BancoPresentacion/src/main/java/com/mycompany/bancopresentacion/BancoPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancopresentacion;
+
+/**
+ *
+ * @author carlo
+ */
+public class BancoPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, github!");
+    }
+}
