fb2970f7469c7df8785572ff4ac93352ff237f98
Carlos <carloshiramsanchez@gmail.com>
2024-02-11 16:33:25 -0700

    Created BancoPersistencia

---
+++ BancoPersistencia/src/main/java/com/mycompany/bancopersistencia/BancoPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancopersistencia;
+
+/**
+ *
+ * @author carlo
+ */
+public class BancoPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("ola, esto deberia estar en github!");
+    }
+}
