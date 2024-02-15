7b57a7c1873aa5fb5295b2ac0463021efff4882a
ChemaArmenta26 <159948311+ChemaArmenta26@users.noreply.github.com>
2024-02-15 00:12:32 -0700

    Cambio de Contraseña conexion.

--- BancoPersistencia/src/main/java/com/mycompany/bancopersistencia/controlador/ControladorPersistencia.java
+++ BancoPersistencia/src/main/java/com/mycompany/bancopersistencia/controlador/ControladorPersistencia.java
@@ -23,7 +23,7 @@ import com.mycompany.bancopersistencia.persistencia.PersistenciaException;
 public class ControladorPersistencia {
     String cadenaConexion = "jdbc:mysql://localhost:3306/banco";
     String usuario = "root";
-    String contrasenia = "chemahermoso123";
+    String contrasenia = "Itson";
     IConexion conexionBD = new Conexion(cadenaConexion, usuario, contrasenia);
     IClienteDAO clienteDAO = new ClienteDAO(conexionBD);
     ICuentaDAO cuentaDAO = new CuentaDAO(conexionBD);
