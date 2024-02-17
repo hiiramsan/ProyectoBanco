f39e7461e74f2ce989ed00005aa6991134389f07
ChemaArmenta26 <159948311+ChemaArmenta26@users.noreply.github.com>
2024-02-17 13:01:51 -0700

    Script de los Triggers
    Creacion del script que contiene los triggers, con el trigger para añadir una cuenta despues de registrar un cliente

---
+++ Triggers.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+
+CREATE TRIGGER CrearCuentaDespuesDeInsertarCliente
+AFTER INSERT ON Clientes
+FOR EACH ROW
+BEGIN
+    DECLARE cuenta_generada INT;
+    
+    -- Generar un número de cuenta aleatorio único
+    REPEAT
+        SET cuenta_generada = FLOOR(RAND() * 900000) + 100000; -- Número de cuenta de 6 dígitos
+    UNTIL NOT EXISTS (SELECT * FROM Cuentas WHERE num_cuenta = cuenta_generada) END REPEAT;
+    
+    -- Insertar la nueva cuenta asociada al cliente
+    INSERT INTO Cuentas (num_cuenta, fecha_apertura, saldo, id_cliente, estado)
+    VALUES (cuenta_generada, CURDATE(), 0, NEW.id_cliente, 'Activa');
+    
+END $$
+
+DELIMITER ;
